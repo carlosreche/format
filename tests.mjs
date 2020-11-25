60586a79655747b96b957d386a93165f59ca2f68
Carlos Reche <carlosreche@gmail.com>
2020-11-25 01:43:55 -0300

    Update tests.mjs

--- tests.mjs
+++ tests.mjs
@@ -1,10 +1,8 @@
 import format from './format.mjs';
 
-format.prototype();
-const log = console.log;
 
 
-log(format.number(1000.5, {
+console.log(format.number(1000.5, {
     thousandsSeparator: '.',
     decimalSeparator: ',',
     decimalDigits: 2
@@ -12,21 +10,21 @@ log(format.number(1000.5, {
 ));
 
 
-log(format.date(new Date('2020-09-05 16:15:00'), {
-    pattern: '{month} dd, yyyy - hh:ii',
+console.log(format.date(new Date('2020-09-05 16:15:00'), {
+    pattern: '{abbrMonth} dd, yyyy - hh:ii',
     fields: {
-      month: date => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()]
+      abbrMonth: date => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                          'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()]
     }
   }
 ));
 
-//--- formatting a messed up text
-log(format.text('   the QUICK browN      fox jumps over the lazy dog', {
+
+console.log(format.text('   the QUICK browN      fox jumps over the lazy dog', {
     clearExtraSpaces: true,
     toLowerCase:      true,
     capitalizeWords:  true,
     truncateSize:     35,
     truncateSuffix:   ' ...'
   }
-)); // output: "The Quick Brown Fox Jumps Over ..." (length = 34)
-
+));
